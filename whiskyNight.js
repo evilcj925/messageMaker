dd10f6aa9f8ea04e88dbcc6cf1a1d56c98f69726
Geoff Buchan <gje.buchan@gmail.com>
2020-11-10 23:46:52 -0700

    complete whiskyNight.js

--- whiskyNight.js
+++ whiskyNight.js
@@ -19,16 +19,56 @@ const whiskies = {
 let pick1 = styleOfWhisky();
 let pick2 = styleOfWhisky();
 let pick3 = styleOfWhisky();
+
+
+//Now i'm creating some transition sentences in the event the picks are duplicate.
 let firstTransition;
 let secondTransition;
 if (pick1 === pick2) {
-     firstTransition = 'For the second bottle, we will once again be sampling a ${pick1} whisky.';
+     firstTransition = `For the second bottle, we will once again be sampling a ${pick1} whisky:`;
 } else {
-     firstTransition = 'The next bottle up is a ${pick2} whisky.';
+     firstTransition = `The next bottle up is a ${pick2} whisky:`;
 };
 if (pick3 === pick2 || pick3 === pick1) {
-    secondTransition = 'The last bottle is once again a ${pick3} whisky.'
+    secondTransition = `The last bottle is once again a ${pick3} whisky:`
 } else {
-    secondTransition = 'Finally, we will be having a ${pick3} whisky.'
+    secondTransition = `Finally, we will be having a ${pick3} whisky:`
 }
+
+
+//Next, we are going to assign bottle names, from the {whiskies} object, to each of the three bottles
+//The first bottle is straightforward, but for bottles 2 & 3 we want to ensure there are no duplicates
+const bottle1 = whiskies[pick1][Math.floor(Math.random() * 5)];
+
+const bottle2picker = () => {
+    let name2 = whiskies[pick2][Math.floor(Math.random() * 5)];
+    if (pick2 === pick1) {
+        
+        while (name2 === bottle1) {
+            name2 = whiskies[pick2][Math.floor(Math.random() * 5)];
+        };
+    };
+    return name2;
+}
+const bottle2 = bottle2picker();
+
+const bottle3picker = () => {
+    let name3 = whiskies[pick3][Math.floor(Math.random() * 5)];
+    if (pick3 === pick1 || pick3 === pick2) {
+        
+        while (name3 === bottle1 || name3 === bottle2) {
+            name3 = whiskies[pick3][Math.floor(Math.random() * 5)];
+        };
+        
+    };
+    return name3;   
+}
+const bottle3 = bottle3picker(); 
+
+//Lastly, lets put it all together in a neat output. 
+console.log(`Welcome to tonight's whisk(e)y tasting! We will be tasting three bottles.`);
+console.log(`First up is a ${pick1} whisky: ${bottle1}.`);
+console.log(`${firstTransition} ${bottle2}.`);
+console.log(`${secondTransition} ${bottle3}. Enjoy!`);
+
    
